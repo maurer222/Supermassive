2922ecfa406c4a275d3de0701d334835bfa384eb
Charles Maurer <maurer-2@hotmail.com>
2021-07-06 09:20:33 -0400

    +Mass and Gravity functionality

--- Supermassive_Mirror/Assets/Scripts/Mass.cs
+++ Supermassive_Mirror/Assets/Scripts/Mass.cs
@@ -1,8 +1,68 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mass : MonoBehaviour
+public class Mass : NetworkBehaviour
 {
+    private float currentMass;
+    private float incomingMass;
 
+    private void Start()
+    {
+        if (gameObject.name.Contains("Player"))
+        {
+            currentMass = 1.2f;
+            incomingMass = currentMass;
+            transform.localScale = new Vector3(currentMass, currentMass, currentMass);
+        }
+        if (gameObject.name.Contains("Star"))
+        {
+            currentMass = Random.Range(.003f, .2f);
+            incomingMass = currentMass;
+            transform.localScale = new Vector3(currentMass, currentMass, currentMass);
+        }
+        if (gameObject.name.Contains("Antimatter"))
+        {
+            currentMass = -1f;
+            incomingMass = currentMass;
+            transform.localScale = Vector3.one;
+        }
+    }
+
+    private void Update()
+    {
+        if (gameObject.name.Contains("Player") && (currentMass < incomingMass))
+        {
+            SmoothSizeIncrease();
+        }
+    }
+
+    void SmoothSizeIncrease()
+    {
+        Vector3 targetMass = new Vector3(incomingMass, incomingMass, incomingMass);
+        Vector3 smoothedMass = Vector3.Lerp(transform.localScale, targetMass * .5f, .01f * Time.deltaTime);
+        currentMass += .7f * Time.deltaTime;
+        transform.localScale = smoothedMass;
+    }
+
+    public float GetMass()
+    {
+        return currentMass;
+    }
+
+    public void SetMass(float mass)
+    {
+        currentMass += mass;
+    }
+
+    public float GetIncomingMass()
+    {
+        return incomingMass;
+    }
+
+    public void SetIncomingMass(float incMass)
+    {
+        incomingMass += incMass;
+    }
 }
