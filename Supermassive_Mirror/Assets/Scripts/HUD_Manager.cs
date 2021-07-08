ac0eda8a8776f65a7050e2c1c11ba5ba37b47041
Charles Maurer <maurer-2@hotmail.com>
2021-07-08 10:57:06 -0400

    +HUD Manager displaying mass, incomingmass, remaining stars

--- Supermassive_Mirror/Assets/Scripts/HUD_Manager.cs
+++ Supermassive_Mirror/Assets/Scripts/HUD_Manager.cs
@@ -8,6 +8,15 @@ public class HUD_Manager : MonoBehaviour
     private float currentMass;
     private float incomingMass;
     private int remainingStars;
+    private Text currentMassText;
+    private Text incomingMassText;
+    private Text remainingStarsText;
+    private void Start()
+    {
+        currentMassText = GameObject.Find("Current Mass").GetComponent<Text>();
+        incomingMassText = GameObject.Find("Incoming Mass").GetComponent<Text>();
+        remainingStarsText = GameObject.Find("Remaining Stars").GetComponent<Text>();
+    }
 
     private void Update()
     {
@@ -16,19 +25,19 @@ public class HUD_Manager : MonoBehaviour
 
     public void SetUICurrentMassText(Mass mass)
     {
-        currentMass = mass.GetMass();
-        GetComponentInChildren<Text>().text = currentMass.ToString();
+        currentMass = ((int)(mass.GetMass() * 100))/100.0f;
+        currentMassText.text = "Current Mass: " + currentMass.ToString();
     }
 
     public void SetUIIncomingMassText(Mass mass)
     {
-        incomingMass = mass.GetIncomingMass();
-        GetComponentInChildren<Text>().text = incomingMass.ToString();
+        incomingMass = ((int)(mass.GetIncomingMass() * 100)) / 100.0f;
+        incomingMassText.text = "Incoming Mass: " + incomingMass.ToString();
     }
 
     public void SetUIRemainingStarsText()
     {
         remainingStars = FindObjectOfType<Star_Spawner>().transform.childCount;
-        GetComponentInChildren<Text>().text = remainingStars.ToString();
+       remainingStarsText.text = "Remaining Stars: " + remainingStars.ToString();
     }
 }
