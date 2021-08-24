991e8e5abb69d20d0cfe1b7edbe8cb66ba9dd99d
Charles Maurer <maurer-2@hotmail.com>
2021-08-24 10:22:13 -0400

    Hooked up all Start Menu Buttons to change scenes

--- Supermassive_Mirror/Assets/Scripts/Scene_Manager.cs
+++ Supermassive_Mirror/Assets/Scripts/Scene_Manager.cs
@@ -1,12 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    public void LoadScene()
+    private static Scene_Manager _instance;
+    private static Scene_Manager Instance {get { return _instance; } }
+
+    public void Awake()
+    {
+        if(_instance != null && _instance != this) { Destroy(this.gameObject); }
+        else { _instance = this; }
+    }
+    public void LoadStartMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadMainGameplayScene()
     {
         SceneManager.LoadScene(1);
+    }
+    public void LoadSettingScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
